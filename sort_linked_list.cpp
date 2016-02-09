e32f81a3e911695d8306ee1e7a54df3295734577
Kenny Liu <nike9926@gmail.com>
2016-02-08 18:55:55 -0800

    Sort linked list by merge sort

---
+++ sort_linked_list.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int num): val(num), next(NULL) {};
+};
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+
+    return;
+}
+
+ListNode* merge2Lists(ListNode* L1, ListNode* L2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* curr = dummy;
+
+    while (L1 && L2) {
+        if (L1->val <= L2->val) {
+            curr->next = L1;
+            L1 = L1->next;
+        } else {
+            curr->next = L2;
+            L2 = L2->next;
+        }
+        curr = curr->next;
+    }
+    if (L1) {
+        curr->next = L1;
+    } else if (L2) {
+        curr->next = L2;
+    }
+    curr = dummy->next;
+    delete dummy;
+
+    return curr;
+}
+
+ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        // Cut this list to two parts
+        fast = slow;
+        slow = slow->next;
+        fast->next = NULL;
+        ListNode* L1 = sortList(head);
+        ListNode* L2 = sortList(slow);
+
+        return merge2Lists(L1, L2);
+}
+
+int main() {
+    ListNode* n1 = new ListNode(5);
+    ListNode* n2 = new ListNode(2);
+    ListNode* n3 = new ListNode(3);
+    ListNode* n4 = new ListNode(6);
+    ListNode* n5 = new ListNode(1);
+    ListNode* n6 = new ListNode(9);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n5->next = n6;
+    ListNode* head = n1;
+
+    cout << "Before sort: ";
+    printList(head);
+    cout << "After sort: ";
+    head = sortList(head);
+    printList(head);
+
+    delete n1;
+    delete n2;
+    delete n3;
+    delete n4;
+    delete n5;
+    delete n6;
+
+    return 0;
+}
